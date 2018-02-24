4675ee93e51c510f4e00bb16ec52ee5824b75c44
co5ta <costamonzili@gmail.com>
2018-02-24 17:56:26 +0100

    Initial commit

---
+++ BattleSwift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  BattleSwift
+//
+//  Created by Co5ta on 22/02/2018.
+//  Copyright © 2018 Co5ta. All rights reserved.
+//
+
+import Foundation
+
+print(" -- BATTLE SWIFT -- ")
